70972f37b6498eab4ebe928737d8b4e724e795ea
hyunjun529 <hyunjun529@gmail.com>
2018-09-10 19:46:36 +0900

    default cross platform game project with Metal & changed AppIcon

---
+++ AMglTF/AMglTF macOS/GameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GameViewController.swift
+//  AMglTF macOS
+//
+//  Created by hyunjun529 on 10/09/2018.
+//  Copyright © 2018 hyunjun529. All rights reserved.
+//
+
+import Cocoa
+import MetalKit
+
+// Our macOS specific view controller
+class GameViewController: NSViewController {
+
+    var renderer: Renderer!
+    var mtkView: MTKView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let mtkView = self.view as? MTKView else {
+            print("View attached to GameViewController is not an MTKView")
+            return
+        }
+
+        // Select the device to render with.  We choose the default device
+        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
+            print("Metal is not supported on this device")
+            return
+        }
+
+        mtkView.device = defaultDevice
+
+        guard let newRenderer = Renderer(metalKitView: mtkView) else {
+            print("Renderer cannot be initialized")
+            return
+        }
+
+        renderer = newRenderer
+
+        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
+
+        mtkView.delegate = renderer
+    }
+}
